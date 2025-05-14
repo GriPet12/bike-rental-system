6993632abbcc0dbffbcc13234dd83a4ed4e8dd24
GriPet <pgrigorcuk0@gmail.com>
2025-05-14 15:25:11 +0300

    Updating the structure to move to server-side rendering

---
+++ ui-service/src/test/java/com/pnu/uiservice/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.uiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
